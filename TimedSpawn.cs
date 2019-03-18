aa264219276f14d8577f752fe6a6cfbb9412f317
CapstoneResearch2019 <48664885+CapstoneResearch2019@users.noreply.github.com>
2019-03-17 23:33:14 -0400

    Create TimedSpawn.cs

---
+++ TimedSpawn.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class TimedSpawn : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public bool stopSpawning = false;
+    public float spawnStartTime;
+    public float spawnRate;
+    public float spawnLimit;   
+    public float topSAE;
+    public float bottomSAE;
+
+    private int saeLevel;
+    private GameObject spawnee;
+    private Transform spawnPos;
+    private CarArray carArray;
+    private Pathfinder pathfinder;
+    private SpeedSearch spd;
+    private Timer timer;
+    private MeanCalculator record;
+
+    private void Awake()
+    {
+        stopSpawning = false;
+        spawnPos = spawnPoint.GetComponent<Transform>();
+        carArray = GameObject.Find("Manager").GetComponent<CarArray>();
+        spd = gameObject.GetComponent<SpeedSearch>();
+        timer = GameObject.Find("Manager").GetComponent<Timer>();
+    }
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", (spawnStartTime + Random.Range(-1.0f, 1.0f)), (spawnRate + Random.Range(-1.0f, 1.0f)));
+    }
+
+    void Update()
+    {
+        if (timer.spawnTimer >= (spawnLimit + spawnStartTime)) stopSpawning = true;
+    }
+
+    public void SpawnObject()
+    {
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+            enabled = false;
+        }
+        if (!stopSpawning)
+        {
+            saeLevel = (int)Random.Range(topSAE, bottomSAE);
+            spawnee = carArray.carArray[saeLevel];
+            GameObject clone = Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+            pathfinder = clone.GetComponent<Pathfinder>();
+            spd = clone.GetComponent<SpeedSearch>();
+            pathfinder.enabled = true;
+            pathfinder.current = gameObject;
+            pathfinder.pointNetwork = gameObject.GetComponent<PointNetwork>();
+            spd.saeLevel = saeLevel;
+        }
+    }
+}
