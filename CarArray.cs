2c3ae4e828173dbcb9ccd2d9383e5ed8a3184690
CapstoneResearch2019 <48664885+CapstoneResearch2019@users.noreply.github.com>
2019-04-09 11:16:14 -0400

    Update CarArray.cs

--- CarArray.cs
+++ CarArray.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class CarArray : MonoBehaviour
 {
-    public GameObject[] carArray; 
-    public GameObject[] destinationArray;
-    [HideInInspector] public int destNum;
+    public GameObject[] carArray; //Contains each variation of the car object, assigned manually in inspector
+    public GameObject[] destinationArray; //Contains each destination object for a given path, assigned manually in inspector
+    [HideInInspector] public int destNum; //Length of destinationArray
 
     void Start()
     {
