5a251430895e0752e234ddfcf14947d46ebf6ef1
CapstoneResearch2019 <48664885+CapstoneResearch2019@users.noreply.github.com>
2019-04-09 13:33:51 -0400

    Update CarArray.cs

--- CarArray.cs
+++ CarArray.cs
@@ -6,10 +6,13 @@ public class CarArray : MonoBehaviour
 {
     public GameObject[] carArray; //Contains each variation of the car object, assigned manually in inspector
     public GameObject[] destinationArray; //Contains each destination object for a given path, assigned manually in inspector
-    [HideInInspector] public int destNum; //Length of destinationArray
+    [HideInInspector] public int destNum; //Length of destinationArray, initialized in Start function
 
+    //Start function is called at the beginning of a program run
     void Start()
     {
         destNum = destinationArray.Length;
     }
 }
+//Used to define global variables in the inspector
+//*ATTACH SCRIPT TO MANAGER*
