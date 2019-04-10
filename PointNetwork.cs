4427ce5c8f98397bd4929259213fc1929ec10157
CapstoneResearch2019 <48664885+CapstoneResearch2019@users.noreply.github.com>
2019-04-10 11:03:21 -0400

    Update PointNetwork.cs

--- PointNetwork.cs
+++ PointNetwork.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class PointNetwork : MonoBehaviour
 {
-    public GameObject current;
-    public GameObject nextPos;
-    public GameObject leftMerge;
-    public GameObject rightMerge;
-    public GameObject altNextPos;
-    public GameObject[] possibleDestArray; 
-    [HideInInspector] public bool stopSpace = false;
-    [HideInInspector] public int destSize;
+    //Initialized in inspector
+    public GameObject current; //Reference point car is moving from
+    public GameObject nextPos; //Reference point car is moving towards
+    public GameObject leftMerge; //Reference point for left merge
+    public GameObject rightMerge; //Reference point for right merge
+    public GameObject altNextPos; //Reference point car may move towards in the case of a branching path
+    public GameObject[] possibleDestArray; //Array of destination points reachable by a car at any given location
+    
+    [HideInInspector] public bool stopSpace = false; //Determines if a point is a stoplight
+    [HideInInspector] public int destSize; //Length of possibleDestArray
 
+    //Start function is called at the beginning of a program run
     void Start()
     {
+        //Initialization
         current = gameObject;
         destSize = possibleDestArray.Length;
     }
 
+    //Update function is called before each frame
     void Update()
     {
         if (stopSpace == false) IconManager.SetIcon(gameObject, IconManager.Icon.DiamondGray);
