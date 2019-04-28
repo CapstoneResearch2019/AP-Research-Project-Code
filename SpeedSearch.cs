3dc8a9d830d0fe4f2ae3dbde9e1882dee8904df6
CapstoneResearch2019 <48664885+CapstoneResearch2019@users.noreply.github.com>
2019-04-28 15:39:57 -0400

    Update SpeedSearch.cs

--- SpeedSearch.cs
+++ SpeedSearch.cs
@@ -4,56 +4,59 @@ using UnityEngine;
 
 public class SpeedSearch : MonoBehaviour
 {
-    public float mySpeed = 5.0f;
-    [HideInInspector] public int saeLevel = 1;
-    [HideInInspector] public GameObject destination;
+    public float mySpeed = 5.0f; //Speed car is currently traveling at
+    [HideInInspector] public int saeLevel = 1; //SAE level
+    [HideInInspector] public GameObject destination; //Destination car attempts to reach
 
-    private Pathfinder path; 
-    private PointNetwork pointNetwork;
-    private CarArray carArray;
-    private float preferredSpeed;
-    private bool isAutomated;
-    private int aggro;
-    [SerializeField] private float followDistance;
-    private float followSpeed;
-    private bool isFollowing = false;
-    private float laneWidth;
-    private float tempFD;
+    private Pathfinder path; //Reference to Pathfinder.cs
+    private PointNetwork pointNetwork; //Reference to PointNetwork.cs
+    private CarArray carArray; //Reference to CarArray.cs
+    private float preferredSpeed; //Speed that the car wants to move at
+    private bool isAutomated; //Determines whether or not a car is automated 
+    private int aggro; //Determines behavior of non-automated cars
+    [SerializeField] private float followDistance; //Space in front of car where other cars can trigger following state 
+    private float followSpeed; //Speed of car in following state
+    private bool isFollowing = false; //Determines whether or not car is in following state
+    private float tempFD; //temporary following distance
     
+    //Start function is called at the beginning of a program run
     void Start()
     {
-        if (saeLevel > 2) isAutomated = true;
-        else isAutomated = false;
-        aggro = (int)Random.Range(1.0f, 10.9f);
-        if (isAutomated) preferredSpeed = 5;
-        else preferredSpeed = 2 + (aggro / 2);
+        if (saeLevel > 2) isAutomated = true; //Cars at SAE levels 3-5 are automated
+        else isAutomated = false; //Cars at SAE levels 0-2 are not automated
+        aggro = (int)Random.Range(1.0f, 10.9f); //Gives car a random value from 1-10
+        if (isAutomated) preferredSpeed = 5; //Automated cars given the same preferredSpeed value
+        else preferredSpeed = 2 + (aggro / 2); //Non-automated cars given a random preferredSpeed value
         carArray = GameObject.Find("Manager").GetComponent<CarArray>();
-        destination = carArray.destinationArray[(int)Random.Range(0, (carArray.destNum - 0.1f))];
+        destination = carArray.destinationArray[(int)Random.Range(0, (carArray.destNum - 0.1f))]; //Assigns a random destination to car
         path = gameObject.GetComponent<Pathfinder>();
         mySpeed = preferredSpeed;
         followSpeed = mySpeed;
     }
 
+    //Update function is called once before every frame of animation
     void Update()
     {
-        if (mySpeed < 0) mySpeed = 0;
-        if (followDistance < 1) followDistance = 1;
+        if (mySpeed < 0) mySpeed = 0; //Speed values cannot be negative
+        if (followDistance < 1) followDistance = 1; //followDistance cannot fall below 1
 
-        if (isAutomated) preferredSpeed = 5;
-        else preferredSpeed = 2 + (aggro / 2);
+        if (isAutomated) preferredSpeed = 5; //Automated cars have the same preferredSpeed value
+        else preferredSpeed = 2 + (aggro / 2); //Non-automated cars have a random preferredSpeed value
 
+        //Sets following distance as a function of speed
         if (isAutomated) tempFD = mySpeed;
         else tempFD = mySpeed - (aggro * 0.2f);
         if (tempFD >= 1) followDistance = tempFD;
         else followDistance = 1;
-        Search();
-        SetSpeed();        
+        
+        Search(); //Function call
+        SetSpeed(); //Function call
     }
 
     void SetSpeed()
     {
-        if (isFollowing) mySpeed = followSpeed;
-        else if (mySpeed != preferredSpeed)
+        if (isFollowing) mySpeed = followSpeed; //Copies speed of car in front when isFollowing is true
+        else if (mySpeed != preferredSpeed) //Car changes its speed to reach its assigned preferredSpeed value
         {
             if (mySpeed > preferredSpeed)
             {
@@ -67,6 +70,7 @@ public class SpeedSearch : MonoBehaviour
         else mySpeed = preferredSpeed;
     }
 
+    //Searches for cars ahead at a certain distance
     void Search()
     {
         Ray ray = new Ray(transform.position + transform.forward * 1.8f, transform.forward);
@@ -88,11 +92,11 @@ public class SpeedSearch : MonoBehaviour
         }
         else
         {
-            //Debug.DrawRay(ray.origin, ray.direction * followDistance, Color.blue);
             isFollowing = false;
         }
     }
 
+    //Checks if there are other cars in a given area to the left of the car
     public bool SafeToMergeL()
     {
         int x = 0;
@@ -104,7 +108,6 @@ public class SpeedSearch : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 3.0f))
             {
                 GameObject leftCar = hit.transform.gameObject;
-                //Debug.DrawRay(ray.origin, ray.direction * 3, Color.green);
                 if (leftCar != null && hit.collider.isTrigger)
                 {
                     x++;
@@ -115,6 +118,7 @@ public class SpeedSearch : MonoBehaviour
         return true;
     }
 
+    //Checks if there are other cars in a given area to the right of the car
     public bool SafeToMergeR()
     {
         int x = 0;
@@ -126,7 +130,6 @@ public class SpeedSearch : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 3.0f))
             {
                 GameObject rightCar = hit.transform.gameObject;
-                //Debug.DrawRay(ray.origin, ray.direction * 3, Color.green);
                 if (rightCar != null && hit.collider.isTrigger)
                 {
                     x++;
@@ -137,6 +140,7 @@ public class SpeedSearch : MonoBehaviour
         return true;
     }
 
+    //Checks if the car needs to merge to the left to reach its destination
     public bool WantToMergeL()
     {
         GameObject searchObj;
@@ -169,6 +173,7 @@ public class SpeedSearch : MonoBehaviour
         return false;
     }
 
+    //Checks if the car needs to merge to the right to reach its destination
     public bool WantToMergeR()
     {
         GameObject searchObj;
